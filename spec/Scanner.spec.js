3b38fb45376c2c1376b51cae56793132000545b6
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2017-06-29 17:47:58 -0400

    Add a test for the scanner, add mocha globals to pkg for standard.

---
+++ spec/Scanner.spec.js
@@ -0,0 +1,31 @@
+const Scanner = require('../src/Scanner')
+const TT = require('../src/types/TokenType')
+
+describe('Lexical Scanner', () => {
+  it('should be able to scan a single line and parse it', () => {
+    const scanner = new Scanner('var hello = "world"')
+
+    expect(scanner.scanTokens()).to.deep.equal([
+      {
+        type: TT.VAR,
+        lexeme: 'var',
+        literal: null,
+        line: 1
+      }, {
+        type: TT.IDENTIFIER,
+        lexeme: 'hello',
+        literal: null,
+        line: 1
+      }, {
+        type: TT.EQUAL,
+        lexeme: '=',
+        literal: null,
+        line: 1
+      }, {
+        type: TT.STRING,
+        lexeme: '"world"',
+        literal: 'world',
+        line: 1
+      }])
+  })
+})
